c48a67eb7c90c0761e728b86db9f20a09c1fcfec
Khanh Duy <thkduy123@gmail.com>
2020-10-29 10:00:48 +0700

    fix error

--- routes/board.js
+++ routes/board.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Board = require('../models/Board');
-const Column = require('../models/column');
-const Card = require('../models/card');
+const Board = require('../models/board.js');
+const Column = require('../models/column.js');
+const Card = require('../models/card.js');
 router.get('/', async (req, res, next) => {
     const boards = await Board.find().populate(
       {
